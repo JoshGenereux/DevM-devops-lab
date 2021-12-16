f60f3515083da8d0ddfd6f26126cf5b3e12e0971
JoshGenereux <josh.genereux13@gmail.com>
2021-12-16 14:08:24 -0700

    added rollbar

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: '849e54d3e1ab497c9b7995e2185135c5',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 app.get('/', (req, res)=>{
     res.sendfile(path.join(__dirname, '/index.html'))
 })
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 8080;
 
